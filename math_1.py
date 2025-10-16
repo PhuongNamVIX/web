f7b73be5b6afaf90848cad43068689bdebb9d4ab
phuongnam <namlienquan3@gmail.com>
2025-10-16 08:21:17 +0700

    'python

---
+++ math_1.py
@@ -0,0 +1,9 @@
+import math
+radius = float(input ("type your radius: "))
+number_pi= math.pi
+circumference= 2*number_pi*radius
+area = number_pi*pow(radius,2)
+print(f"The circumference of circle equal: {round(circumference,2)}cm")
+print(f"The area of circle equal: {round(area,2)}cm²")
+
+
---
+++ print.py
@@ -0,0 +1,36 @@
+#Ex1
+# length=float(input("Input the lenght of Rectangle: "))
+# width=input("Input the width of Rectangle: ")
+# width=float(width)
+
+# area = width * length
+
+# print(f"The Area is: {area} cm² ") # giữ "alt" bấm từng số 0178 là ra cm vuông
+
+#Ex2
+# item=input("What item would you like to buy: ")
+# price= float(input("What is the price?: "))
+# quantity= int(input("How many would you like?: "))
+
+# total=price*quantity
+
+# print(f"You have bought {quantity} x {item}/s")
+# print(f"Your total is: ${total}")
+
+#Ex3
+
+# print(f"Today I went to a ..... zoo")
+# place= input("What the word do you think: ")
+# result ="HCM"
+# if place==result:
+#     print("Your answer is right")
+# else:
+#     print("sorry")
+
+
+import math
+
+radius = float(input ("type your radius2: "))
+number_pi= math.pi
+result= 2*number_pi*radius
+print(f"The result is: {result}")
